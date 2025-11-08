6f55dfaab16ac2096547475d8452d35841c8e449
Mahamudun Nabi Siam <mahamudunn1005@gmail.com>
2025-11-08 18:47:50 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -24,6 +24,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { PlusCircle, Edit, Trash2, Loader2, Search, LogOut } from 'lucide-react';
 import Link from 'next/link';
