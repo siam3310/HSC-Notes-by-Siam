11054854347a2fdd7ae304b6f7ccd11e668b8b4f
Mahamudun Nabi Siam <mahamudunn1005@gmail.com>
2025-11-10 03:30:20 +0000

    Try fixing this error: `Console Error: Error: An unexpected response was

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      executionTimeout: 120, // 120 seconds
+    },
+  },
   serverActions: {
     bodySizeLimit: '30mb',
   },
