112cc358f5d0ef5278ed9bf0aaff4c58891a3494
Mahamudun Nabi Siam <164713293+siam3310@users.noreply.github.com>
2025-11-13 12:22:59 +0600

    Update metadata title and description
    Updated the title and description in metadata to include the author's name and a more detailed description.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { AppLayout } from '@/components/AppLayout';
 
 export const metadata: Metadata = {
-  title: 'HSC Notes',
-  description: 'A comprehensive collection of study notes for HSC students.',
+  title: 'HSC Notes by Siam',
+  description: 'A comprehensive collection of study notes for HSC students. Build with love by Mahamudun Nabi Siam',
 };
 
 export default function RootLayout({
@@ -15,6 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <meta name="google-site-verification" content="yAFfTwqZrPnN9SKdzxCNipM_H63nQuqk8YkIZDlDOcc" />
       <body className="antialiased font-sans">
         <AppLayout>
             {children}
