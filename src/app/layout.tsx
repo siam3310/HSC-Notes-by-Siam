69f9b2c3d253caa8a420b96f43fb67ea646798a0
Mahamudun Nabi Siam <164713293+siam3310@users.noreply.github.com>
2025-11-14 10:07:10 +0600

    Update metadata titles and descriptions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { AppLayout } from '@/components/AppLayout';
 
 export const metadata: Metadata = {
-  title: 'HSC Notes by Siam',
-  description: 'A comprehensive collection of study notes for HSC students. Build with love by Mahamudun Nabi Siam',
+  title: 'HSC Notes',
+  description: 'A comprehensive collection of study notes for HSC students. Made with love by Mahamudun Nabi Siam',
 };
 
 export default function RootLayout({
@@ -17,16 +17,16 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <meta name="google-site-verification" content="yAFfTwqZrPnN9SKdzxCNipM_H63nQuqk8YkIZDlDOcc" />
       <title>HSC Notes by Siam</title>
-<meta name="title" content="HSC Notes by Siam" />
+<meta name="title" content="HSC Notes " />
 <meta name="description" content="A comprehensive collection of study notes for HSC students. Build with love by Mahamudun Nabi Siam" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://hscnotes.vercel.app/" />
-<meta property="og:title" content="HSC Notes by Siam" />
+<meta property="og:title" content="HSC Notes " />
 <meta property="og:description" content="A comprehensive collection of study notes for HSC students. Build with love by Mahamudun Nabi Siam" />
 <meta property="og:image" content="https://hscnotes.netlify.app/favicon.ico" />
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://hscnotes.vercel.app/" />
-<meta property="twitter:title" content="HSC Notes by Siam" />
+<meta property="twitter:title" content="HSC Notes" />
 <meta property="twitter:description" content="A comprehensive collection of study notes for HSC students. Build with love by Mahamudun Nabi Siam" />
 <meta property="twitter:image" content="https://hscnotes.netlify.app/favicon.ico" />
 
